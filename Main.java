f8617a6b81fc11c6e03d47405b6c831f705f932d
Marcos Manzanares <marcos.manzanares@patelhs.org>
2021-02-02 13:59:44 +0000

    Scanner, questions, print statements, and scanner variables are done :)

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
+    System.out.println("Hello! the Jerseys are finally in! We need your name and favorite number for your soccer jersey!");
     Scanner scan = new Scanner(System.in);
-
+    System.out.println("What is your name??");
+    String playersName = scan.nextInt();
+    System.out.println("What is your Favorite number?");
+    int favoriteNumber = scan.nextInt();
 
 
   }
