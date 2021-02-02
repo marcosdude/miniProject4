7c6a1881f9f0b39616bb033bce44169172001b47
Marcos Manzanares <marcos.manzanares@patelhs.org>
2021-02-02 13:38:10 +0000

    adding the scanner

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
 
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    
+
 
   }
 }
