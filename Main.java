8161cdbe4ac5de39d94afbc9cfdec8005aea54ed
Marcos Manzanares <marcos.manzanares@patelhs.org>
2021-02-03 22:26:58 +0000

    Finished my print statements.

--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
-    System.out.println("Hello! the Jerseys are finally in! We need your name and favorite number for your soccer jersey!");
+    System.out.println("Hello! The Jerseys are finally in! We need your name and favorite number for your soccer jersey!");
     Scanner scan = new Scanner(System.in);
     System.out.println("What name would you like on your jersey?");
     String playersName = scan.next();
     System.out.println("What number would you like on your jersey?");
     int favoriteNumber = scan.nextInt();
 
-  System.out.println("This is your information.");
+  System.out.println("This is your Jersey.");
 
   String Name = playersName;
-   System.out.println(Name);
+   System.out.println("The name on your jersey would be, " + Name + ".");
 
   int Number = favoriteNumber;
-   System.out.println(Number);
+   System.out.println("The number on your jersey would be, " + Number + ".");
   }
 
 
